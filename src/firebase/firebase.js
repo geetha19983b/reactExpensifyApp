3d521140da10e4a15c25f9b6bb2fae74561afdcf
geetha <geethainfy@gmail.com>
2020-06-08 13:26:33 +0530

    env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,136 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref().set({
+//   name: 'Andrew Mead',
+//   age: 26,
+//   isSingle: false,
+//   location: {
+//     city: 'Philadelphia',
+//     country: 'United States'
+//   }
+// }).then(() => {
+//   console.log('Data is saved!');
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// });
+
+// database.ref().set('This is my data.');
+
+// database.ref('age').set(27);
+// database.ref('location/city').set('New York');
+
+// database.ref('attributes').set({
+//   height:56,
+//   weight:69
+// }).then(() => {
+//   console.log('attribute saved');
+// }).catch((e) => {
+//   console.log('attribute save failed',e);
+// });
+
+// database.ref('isSingle')
+//   .remove()
+//   .then(() => {
+//     console.log('Data was removed');
+//   }).catch((e) => {
+//     console.log('Did not remove data', e);
+//   });
+
+
+// database.ref().set({
+//   name: 'Andrew Mead',
+//   age: 26,
+//   stressLevel: 6,
+//   job: {
+//     title: 'Software developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'Philadelphia',
+//     country: 'United States'
+//   }
+// }).then(() => {
+//   console.log('Data is saved!');
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// });
+
+// database.ref().update({
+//   'stressLevel' :9,
+//   'job/company':'Amazon',
+//   'location/city':'Seattle'
+
+// });
+
+// database.ref().on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// })
+
+
+
+// Setup "expenses" with three items (description, note, amount, createdAt)
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 976123498763
+// });
+
+// database.ref('expenses').push({
+//   description: 'Phone bill',
+//   note: '',
+//   amount: 5900,
+//   createdAt: 976123498763
+// });
+
+// database.ref('expenses').push({
+//   description: 'Food',
+//   note: '',
+//   amount: 1200,
+//   createdAt: 976123498763
+// });
+
+
+// child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 976123498763
+// });
+
+
+
